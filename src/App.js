5106cc208a38810b2c7c5e67609952921c2dc802
Victor Tsuka <victortsuka@hotmail.com>
2022-02-17 18:06:22 -0300

    1st Push - estruturação da page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import StyledHome from "./components/produtos/styledHome/StyledHome"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default class App extends React.Component {
+    state = {
+
+    }
+    render() {
+
+
+        return ( <
+            >
+            <
+            StyledHome / >
+            <
+            />
+        );
+    }
 }
 
-export default App;
+
+
+
+
+// Enunciado:
+// Caros alunos,
+// Estou querendo montar um e-commerce de itens espaciais e vocês serão responsáveis por essa construção. 
+// Pretendo montar diferentes linhas de produtos.
+//  As possíveis linhas de produto são:
+// - Satélites antigos;
+// - Roupas espaciais;
+// - Camisetas com estampas divertidas relacionadas ao espaço;
+// - Viagens espaciais;
+// - Meteoritos / aerolitos como pedras preciosas;
+// - Brinquedos e produtos infantis temáticos;
+// - Naves / Foguetes / Ônibus espaciais;
+// Escolham uma linha dentro do grupo de vocês, e criem um e-commerce de acordo com as especificações.
+// O prazo é até sexta, 23h59. Sem atrasos.
+// Como de costume, espero uma entrega no mínimo impecável.
+// Att.
